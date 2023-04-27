1033a367ea204c0fe9e54fd6f1a00ee00ce010ab
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-04-26 20:40:56 -0400

    remove old types

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,10 +1,6 @@
 import Link from 'next/link';
 import { getDictionary } from '@/dictionaries/dictionaries';
 
-import type { NavItem } from '@/types/nav';
-import { cn } from '@/lib/utils';
-import { Input } from '../ui/input';
-
 export function Footer() {
   const dict = getDictionary(); // en
 
