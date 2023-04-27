97e3b74d4732f6454476cf9cd4a71bd3e5c726cf
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-04-26 21:58:43 -0400

    more colors in spectrum

--- components/flags/spectrum.tsx
+++ components/flags/spectrum.tsx
@@ -10,6 +10,18 @@ const flagColors = [
     color: '#ffffff',
   },
   {
+    name: 'white',
+    color: '#f5f5f5',
+  },
+  {
+    name: 'white',
+    color: '#eaeaea',
+  },
+  {
+    name: 'white',
+    color: '#d9d9d9',
+  },
+  {
     name: 'red',
     color: '#ef4444',
   },
@@ -22,12 +34,60 @@ const flagColors = [
     color: '#7f1d1d',
   },
   {
+    name: 'red',
+    color: '#f05252',
+  },
+  {
+    name: 'red',
+    color: '#e32626',
+  },
+  {
+    name: 'red',
+    color: '#c21818',
+  },
+  {
+    name: 'red',
+    color: '#a51a1a',
+  },
+  {
+    name: 'red',
+    color: '#9c1f1f',
+  },
+  {
+    name: 'red',
+    color: '#841e1e',
+  },
+  {
+    name: 'red',
+    color: '#6a2020',
+  },
+  {
+    name: 'red',
+    color: '#581b1b',
+  },
+  {
     name: 'orange',
     color: '#ea580c',
   },
   {
     name: 'orange',
     color: '#9a3412',
+  },
+  {
+    name: 'orange',
+    color: '#f16a1d',
+  },
+  {
+    name: 'orange',
+    color: '#d6530a',
+  },
+  {
+    name: 'orange',
+    color: '#b8460e',
+  },
+  {
+    name: 'orange',
+    color: '#ff7a33',
   },
   {
     name: 'yellow',
@@ -42,6 +102,26 @@ const flagColors = [
     color: '#ca8a04',
   },
   {
+    name: 'yellow',
+    color: '#ffd32a',
+  },
+  {
+    name: 'yellow',
+    color: '#f4c425',
+  },
+  {
+    name: 'yellow',
+    color: '#d9b006',
+  },
+  {
+    name: 'yellow',
+    color: '#ebdd3d',
+  },
+  {
+    name: 'yellow',
+    color: '#b69400',
+  },
+  {
     name: 'green',
     color: '#22c55e',
   },
@@ -54,6 +134,38 @@ const flagColors = [
     color: '#166534',
   },
   {
+    name: 'green',
+    color: '#2ad573',
+  },
+  {
+    name: 'green',
+    color: '#1e9a4b',
+  },
+  {
+    name: 'green',
+    color: '#0f8c3a',
+  },
+  {
+    name: 'green',
+    color: '#29e08d',
+  },
+  {
+    name: 'green',
+    color: '#0c7a31',
+  },
+  {
+    name: 'green',
+    color: '#126e42',
+  },
+  {
+    name: 'green',
+    color: '#1a5a3a',
+  },
+  {
+    name: 'green',
+    color: '#104c28',
+  },
+  {
     name: 'blue',
     color: '#60a5fa',
   },
@@ -64,6 +176,50 @@ const flagColors = [
   {
     name: 'blue',
     color: '#1e40af',
+  },
+  {
+    name: 'blue',
+    color: '#4b9cf9',
+  },
+  {
+    name: 'blue',
+    color: '#2f74e7',
+  },
+  {
+    name: 'blue',
+    color: '#0f5edc',
+  },
+  {
+    name: 'blue',
+    color: '#73b0ff',
+  },
+  {
+    name: 'blue',
+    color: '#1947c1',
+  },
+  {
+    name: 'blue',
+    color: '#153c9e',
+  },
+  {
+    name: 'blue',
+    color: '#1a338b',
+  },
+  {
+    name: 'blue',
+    color: '#102767',
+  },
+  {
+    name: 'black',
+    color: '#0d0d0d',
+  },
+  {
+    name: 'black',
+    color: '#1a1a1a',
+  },
+  {
+    name: 'black',
+    color: '#1e1e1e',
   },
   {
     name: 'black',
@@ -87,7 +243,7 @@ export function Spectrum() {
         className="flex h-full w-full cursor-pointer items-center justify-center"
         onClick={() => clickColor()}
       >
-        <Icons.circleSlashed className="h-12 w-12" />
+        <Icons.circleSlashed className="h-12 w-12 mx-4" />
       </div>
       {flagColors.map((color) => (
         <div
